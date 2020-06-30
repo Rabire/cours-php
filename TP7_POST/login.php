4921b2d44f12f43b1f20c15b23071b1fd2973ae8
Rabire <rabireh@outlook.fr>
2020-06-30 14:35:39 +0200

    TP fonctionnel sans CSS

--- TP7_POST/login.php
+++ TP7_POST/login.php
@@ -6,10 +6,15 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <h1>Connexion</h1>
     <form action="verif_login.php" method="post">
         <p>Votre nom d'utilisateur: <input type="text" name="nom_utilisateur" /></p>
         <p>Votre mot de passe : <input type="password" name="mot_de_passe" /></p>
-        <p><input type="submit" value="Verrifier"></p>
+        <p><input type="submit" value="Verifier"></p>
     </form>
+
+    <?php
+        echo "<h2>" . $_GET['message'] . "</h2>";
+    ?>
 </body>
 </html>
---
+++ TP7_POST/verif_login.php
@@ -0,0 +1,20 @@
+<?php
+    $nom_utilisateur = $_POST["nom_utilisateur"];
+    $mot_de_passe = $_POST["mot_de_passe"];
+
+    function identifiants_correct() {
+        global $nom_utilisateur, $mot_de_passe; // permet d'acceder aux variables qui sont en dehors de la fonction
+        if ($nom_utilisateur === "Dupont" && $mot_de_passe === "alibaba") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if (identifiants_correct()) {
+        echo "<h1>LOGIN CORRECT</h1>";
+    } else {
+        header('Location: login.php?message=Identifiant ou mot de passe incorrect');
+    }
+
+?>
