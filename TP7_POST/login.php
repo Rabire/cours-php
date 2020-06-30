7e75fbafb9b89ffcd9d169f26200812de7c4e8ba
Rabire <rabireh@outlook.fr>
2020-06-30 14:36:13 +0200

    css ajouté

--- TP7_POST/login.php
+++ TP7_POST/login.php
@@ -8,13 +8,14 @@
 <body>
     <h1>Connexion</h1>
     <form action="verif_login.php" method="post">
-        <p>Votre nom d'utilisateur: <input type="text" name="nom_utilisateur" /></p>
-        <p>Votre mot de passe : <input type="password" name="mot_de_passe" /></p>
-        <p><input type="submit" value="Verifier"></p>
+        <input type="text" name="nom_utilisateur" placeholder="Nom d'utilisateur"/>
+        <input type="password" name="mot_de_passe" placeholder="Mot de passe"/>
+        <p><input type="submit" class="btn" value="Verifier"></p>
     </form>
 
     <?php
         echo "<h2>" . $_GET['message'] . "</h2>";
     ?>
+    
 </body>
 </html>
